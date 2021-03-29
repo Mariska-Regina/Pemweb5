feec2779b3deb5aa80eea7563ad1878428bfc4d4
Mariska-Regina <79780537+Mariska-Regina@users.noreply.github.com>
2021-03-29 22:09:28 +0700

    Add files via upload

---
+++ postRequestAct.php
@@ -0,0 +1,7 @@
+<?php
+// Menampilkan output nama yang telah diinputkan pada postRequest.php
+echo "<center> Nama: ".$_REQUEST['nama']."</center><br>";
+
+// Menampilkan output email yang telah diinputkan pada postRequest.php
+echo "<center> Email: ".$_REQUEST['email']."</center><br>";
+?>
